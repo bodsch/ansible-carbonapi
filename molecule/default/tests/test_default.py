57c1f5a2ddd128b28c4c63eeb9e3feea489f0b58
Bodo Schulz <bodo.schulz@tallence.com>
2020-03-11 16:05:12 +0100

    new test infrastructure

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -24,7 +24,9 @@ def test_directories(host, dirs):
 
 
 @pytest.mark.parametrize("files", [
-    "/etc/carbonapi/carbonapi.yaml"
+    "/etc/carbonapi/carbonapi.yaml",
+    "/etc/carbonapi/graphiteWeb.yaml",
+    "/etc/carbonapi/graphTemplates.yaml"
 ])
 def test_files(host, files):
     f = host.file(files)
@@ -32,9 +34,31 @@ def test_files(host, files):
     assert f.is_file
 
 
-# def test_user(host):
-#     assert host.group("jolokia").exists
-#     assert host.user("jolokia").exists
-#     assert "jolokia" in host.user("jolokia").groups
-#     assert host.user("jolokia").shell == "/sbin/nologin"
-#     assert host.user("jolokia").home == "/opt/jolokia"
+def test_solr_service(host):
+    service = host.service("carbonapi")
+
+    # if( service.__class__.__name__ != 'SysvService' ):
+    assert service.is_enabled == True
+    assert service.is_running == True
+
+
+@pytest.mark.parametrize("ports", [
+    '127.0.0.1:8088'
+])
+def test_open_port(host, ports):
+    for i in host.socket.get_listening_sockets():
+        print( i )
+
+    solr = host.socket("tcp://{}".format(ports))
+    assert solr.is_listening
+
+
+def test_storage_schemas(host):
+
+    config_file = "/etc/carbonapi/carbonapi.yaml"
+    content = host.file(config_file).content_string
+
+    assert 'listen: "localhost:8088"' in content
+    assert 'host: "127.0.0.1:2003"' in content
+    assert 'interval: "30s"' in content
+    assert '"http://127.0.0.1:8081"' in content
