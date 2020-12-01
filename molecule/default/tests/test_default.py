a295bc90c2a89e63f16b8df12560936db80e1aa9
Bodo Schulz <bodo@boone-schulz.de>
2020-12-01 07:45:53 +0100

    test with tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,9 +8,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 @pytest.fixture()
-def AnsibleDefaults():
-    with open("../../defaults/main.yml", 'r') as stream:
-        return yaml.load(stream)
+def get_vars(host):
+    defaults_files = "file=../../defaults/main.yml name=role_defaults"
+    vars_files = "file=../../vars/main.yml name=role_vars"
+
+    ansible_vars = host.ansible(
+        "include_vars",
+        defaults_files)["ansible_facts"]["role_defaults"]
+
+    ansible_vars.update(host.ansible(
+        "include_vars",
+        vars_files)["ansible_facts"]["role_vars"])
 
 
 @pytest.mark.parametrize("dirs", [
